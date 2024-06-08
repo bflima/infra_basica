af552b551f996324dcc353a0a16cfe9a119f0ca4
Bruno Lima <47477458+bflima@users.noreply.github.com>
2024-06-08 14:56:36 -0300

    Update dhcp.sh
    Atualização 08/06/2024

--- dhcp.sh
+++ dhcp.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Descomentar a linha abaixo para ativar debug
+# set -xeuo pipefail
+
 ## INFO ##
 ## NOME.............: dhcp.sh
 ## VERSÃO...........: 1.0
@@ -8,28 +11,29 @@
 ## ESCRITO POR......: Bruno Lima
 ## E-MAIL...........: bruno@lc.tec.br
 ## DISTRO...........: Rocky GNU/Linux
+## VERSÃO HOMOLOGADA: 8 e 9 
 ## LICENÇA..........: GPLv3
 ## Git Hub..........: https://github.com/bflima
 
 # Funções
 ################################################################################
-# Função para mostrar mensagem de erro
+# Função para exibir mensagem de erro, usando o programa whiptail.
 _MSG_ERRO_INFO() { clear ; whiptail --title "Erro" --msgbox "Erro" 0 0 ; exit 20 ; }
 
 ################################################################################
-# Função para mostrar mensagem de erro e sair do script com código de erro 30
+# Função para exibir mensagem de erro e sair do script com código de erro 30.
 _MSG_SAIR(){ clear ; whiptail --title "Aviso" --msgbox "$1" 0 0 ; exit 30 ; }
 
 ################################################################################
-# Função para verificar se o script está com privilegios de root, Caso não estiver finaliza o programa.
+# Função para verificar se o script pode ser executado com privilegios de root.
 _VERIFICAR_ROOT(){ [[ "$EUID" -eq 0 ]] || _MSG_ERRO_INFO "Necessita permissão de root" ; }
 
 ################################################################################
-# Função para verificar o sistema operacional Rocky Linux e versão
+# Função para verificar o sistema operacional Rocky Linux.
 _VERIFICAR_OS(){ grep -Eoq 'centos|rocky|rhel' /etc/os-release|| _MSG_ERRO_INFO "Sistema operacional não homologado, Favor usar Rocky Linux" ; }
 
 ################################################################################
-# Função para validar endereço ip usando ipcalc
+# Função para validar endereço ip usando o programaa externo ipcalc.
 _VALIDAR_IP()
 {
   which ipcalc 1> /dev/null || yum -y install ipcalc 1> /dev/null
@@ -39,7 +43,7 @@ _VALIDAR_IP()
 }
 
 ################################################################################
-# Função para validar endereço da interface de rede
+# Função para selecionar a interface de rede. (Ex: enp0s3)
 _INTERFACE()
 {
   NOME_IFACE_TMP=$(ip -o -4 route show to default | awk '{print $5}' | uniq)
@@ -74,6 +78,7 @@ _INTERFACE()
 }
 
 ################################################################################
+
 # Inicio do script
 _VERIFICAR_ROOT
 _VERIFICAR_OS
@@ -84,16 +89,20 @@ ISSUENET=$(find /etc/ -iname issue.net)
 MOTD=$(find /etc/ -iname motd)
 MSG_BANNER='Acesso ao sistema monitorado'
 
-# Hardening no serviço de identificação
+# Alterar a mensagem padrão para acesso ao sistema por ssh.
 echo "$MSG_BANNER" > "$ISSUE"
 echo "$MSG_BANNER" > "$ISSUENET"
 echo "$MSG_BANNER" > "$MOTD"
 
 
+# Verificar se firewalld está ativo, cadastrar um regra para permitir o serviço de dhcp(UDP porta 67,68)
+firewall-cmd --state | grep -iq running && { firewall-cmd --zone=public --add-service=dhcp --permanent ; firewall-cmd --reload ; }
+
+# Realizar confirmação de instalação do serviço
 if whiptail --title "Aviso" --yesno "Deseja instalar e configurar o serviço dhcp? " 10 50 ; then yum update -y ; yum upgrade -y ; yum install dhcp-server -y ; else _MSG_SAIR 'Operação Cancelada'; fi 
 
 clear
-# Gateway da rede
+# Informar o gateway da rede
 GW_ATUAL=$(ip -o -4 route show to default | awk '{print $3}' | head -n 1)
 GW=$(whiptail --title "Favor inserir o default gateway"\
               --inputbox "[$GW_ATUAL]" --fb 10 60 3>&1 1>&2 2>&3) || _MSG_SAIR 'Operação cancelada'
@@ -101,7 +110,7 @@ GW=$(whiptail --title "Favor inserir o default gateway"\
 GW=${GW:=$GW_ATUAL}
 _VALIDAR_IP "$GW"
 
-# Ip da rede e máscara de rede
+# Informar o ip e máscara de rede
 IP_REDE_ATUAL=$(ip route | tail -n +2 | awk '{print $1}')
 IP_REDE=$(whiptail --title "Favor inserir o endereço IP e netmask:"                                                     \
                    --inputbox "Exemplo:\n192.168.0.0/24\n192.168.0.0/255.255.255.0\n\nRede Atual: [$IP_REDE_ATUAL]"     \
@@ -109,40 +118,40 @@ IP_REDE=$(whiptail --title "Favor inserir o endereço IP e netmask:"            
 IP_REDE=${IP_REDE:=$IP_REDE_ATUAL}
 _VALIDAR_IP "$IP_REDE"
 
-# Escopo inicial
+# Informar o escopo inicial
 IP_INI_100=$(ip route | tail -n +2 | awk '{print $1}' | cut -d '/' -f1 | sed -r 's/([0-9.]+)([0-9]$)/\1100/g')
 IP_INI=$(whiptail --title "Favor inserir o endereço IP de escopo inicial:"      \
                    --inputbox "Exemplo:\n[$IP_INI_100]"                         \
                    --fb 15 60 3>&1 1>&2 2>&3) || _MSG_SAIR 'Operação Cancelada'
-IP_INI=${IP_INI:=$(echo $IP_INI_100)}
+IP_INI=${IP_INI:=$IP_INI_100}
 _VALIDAR_IP "$IP_INI"
 
-# Escopo final
+# Informar o escopo final
 IP_FIM_200=$(ip route | tail -n +2 | awk '{print $1}' | cut -d '/' -f1 | sed -r 's/([0-9.]+)([0-9]$)/\1200/g')
 IP_FIM=$(whiptail --title "Favor inserir o endereço IP de escopo final:"        \
                    --inputbox "Exemplo:\n[$IP_FIM_200]"                         \
                    --fb 15 60 3>&1 1>&2 2>&3) || _MSG_SAIR 'Operação Cancelada'
-IP_FIM=${IP_FIM:=$(echo $IP_FIM_200)}
+IP_FIM=${IP_FIM:=$IP_FIM_200}
 _VALIDAR_IP "$IP_FIM"
 
-# DNS primario
+# Informar o DNS primario
 DNS_PRI=$(whiptail --title "Favor inserir o endereço DNS INICIAL:"              \
                    --inputbox "Exemplo:\n[1.1.1.1]"                             \
                    --fb 15 60 3>&1 1>&2 2>&3) || _MSG_SAIR 'Operação Cancelada'
 DNS_PRI=${DNS_PRI:=1.1.1.1}
 _VALIDAR_IP "$DNS_PRI"
 
-# DNS secundário
+# Informar o DNS secundário
 DNS_SEC=$(whiptail --title "Favor inserir o endereço DNS INICIAL:"              \
                    --inputbox "Exemplo:\n[8.8.8.8]"                             \
                    --fb 15 60 3>&1 1>&2 2>&3) || _MSG_SAIR 'Operação Cancelada'
 DNS_SEC=${DNS_SEC:=8.8.8.8}
 _VALIDAR_IP "$DNS_SEC"
 
-# Interface de escuta para o serviço dhcp
+# Selecionar interface de rede disponível para serviço dhcp
 INTERFACE=$(_INTERFACE)   || _MSG_SAIR "$SAMBA_ERRO_MSG"
 
-# Cadastrar dominio
+# Informar o dominio
 DOMINIO=$(whiptail --title "Favor inserir o dominio:"  \
                    --inputbox "Exemplo:\nempresa.corp" \
                    --fb 15 60 3>&1 1>&2 2>&3) || _MSG_SAIR 'Operação Cancelada'
@@ -152,7 +161,7 @@ DOMINIO=${DOMINIO:=$(hostname -f)}
 BROADCAST=$(ipcalc -b "$IP_REDE")
 NETMASK=$(ipcalc   -m "$IP_REDE")
 
-# Verificar dados fornecidos
+# Verificar dados fornecidos estão corretos
 if ! whiptail --title "Dados informados pelo usuario" \
   --yesno "Os dados estao corretos ?\n
   ENDEREÇO DE REDE...=$IP_REDE
@@ -166,11 +175,22 @@ if ! whiptail --title "Dados informados pelo usuario" \
   NAMESERVER 2.......=$DNS_SEC
   DOMINIO............=$DOMINIO" --fb 0 0
 then
-  _MSG_SAIR "$SAMBA_ERRO_MSG"
+  _MSG_SAIR 'Operação Cancelada'
 fi
+
+# Verificar se interface de rede está com ip fixo para o serviço
+DHCP_ENABLED=$(find /etc/ -iname "*""$INTERFACE""*")
+MSG="Favor configurar ip fixo para o serviço dhcp na inteface de rede $INTERFACE"
+grep -E 'method=auto|dhcp' "$DHCP_ENABLED" && whiptail --title "Alerta:" --msgbox "$MSG" --fb 0 0  3>&1 1>&2 2>&3
+
+# Verificar se o endreço de ip inicial é menor que o final
+[[ "${IP_INI##*\.}" -gt "${IP_FIM##*\.}" ]] && _MSG_SAIR 'Escopo final menor que o inicial'
 
 # Arquivo de configuração do dhcp
 DHCP=$(find /etc/ -iname dhcpd.conf)
+
+# Realizar backup do arquivo de configuração inical
+\cp "$DHCP"{,.orig}
 
 cat > "$DHCP" << EOF
 # 691200 segundos (8 dias)
@@ -192,7 +212,7 @@ subnet ${IP_REDE%%/*} netmask ${NETMASK##*=} {
         max-lease-time 791200;
 }
 
-# Reservas ddescomentar para cadastro e uso as linhas abaixo
+# Reservas descomentar para cadastro e uso as linhas abaixo
 
 # host cliente-interno {
 #       hardware ethernet 08:00:27:0b:91:4c;
@@ -201,4 +221,6 @@ subnet ${IP_REDE%%/*} netmask ${NETMASK##*=} {
 EOF
 
 # Reiniciar o serviço
-if whiptail --title "Aviso" --yesno "Deseja iniciar serviço dhcp? " 10 50 ; then systemctl enable dhcpd ; systemctl restart dhcpd ; else _MSG_SAIR 'Operação Cancelada'; fi 
+if whiptail --title "Aviso" --yesno "Deseja iniciar serviço dhcp? " 10 50 ; then systemctl restart NetworkManager; systemctl enable dhcpd ; systemctl restart dhcpd ; else _MSG_SAIR 'Operação Cancelada'; fi 
+
+# Fim do script
